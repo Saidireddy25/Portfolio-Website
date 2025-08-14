33e63e16119588f80099d2bc9889bdba3304c21a
Saidi Reddy Karnati <saidreddyk2545@gmail.com>
2025-08-14 15:14:55 -0500

    Initial commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -33,32 +33,14 @@ export default function Projects() {
       tag: 'Sales Analytics',
     },
     {
-      title: 'On Demand Q/A Bot',
+      title: 'Film Rental Analysis',
       description:
-        'Implemented a privacy-focused Q&A Bot for network security courses, showcasing problem-solving skills using Python and contributing to the open-source community. This initiative allowed for local storage and processing of the Q&A model and course details directly on users’ devices, prioritizing data privacy.The Q&A Bot project, a testament to these skills, achieved outstanding performance by consistently providing accurate and reliable responses to user queries, highlighting a successful integration of innovative solutions.',
-      link: 'https://github.com/maturimonika/-OnDemand-Professor-Q-A-Bot-.git',
-      image: '/qabot.png',
-      techStack: ['Python', 'Hugging Face', 'GitHub Actions','Vector Search','Pinecone','HuggingFace Transformers','Scikit-learn'],
-      tag: 'NLP',
-    },
-    {
-      title: 'Age and Gender Detection',
-      description:
-        'Developed a real-time age group and gender prediction system using CNNs in TensorFlow and Keras, with OpenCV for facial detection and preprocessing. Leveraged transfer learning with VGG16 to boost accuracy across diverse demographics. Implemented image augmentation for robust training and evaluated model performance using accuracy, precision, recall, and confusion matrix. Deployed the model with webcam integration and a user-friendly UI for live biometric predictions.',
-      link: 'https://github.com/prasanthi613/AGE-AND-GENDER-PREDICTION-PROJECT.git',
-      image: '/agegenn.png',
-      techStack: ['Python','TensorFlow', 'Keras', 'OpenCV','NumPy','Confusion Matrix','Scikit-learn','Matplotlib','Jupyter Notebook'],
-      tag: 'AI/ML',
-    },
-    {
-      title: 'Paradise Nursery',
-      description:
-        'A responsive e-commerce web application for browsing and purchasing houseplants. Built with HTML, CSS, and vanilla JavaScript, it features dynamic cart functionality with real-time total updates, modular code structure, and in-memory state management. Includes categorized product listings, interactive quantity adjustments, and responsive design for a smooth user experience across devices. Demonstrates strong frontend skills in DOM manipulation, event handling, and client-side logic.',
-      link: 'https://github.com/prasanthi613/ParadiseNursery.git',
-      image: '/plant.png',
-      techStack: ['HTML', 'CSS', 'JavaScript','Responsive Web Design','Git','Modular JavaScript Structure','DOM Manipulation'],
-      tag: 'UI Demo',
-    },
+        'This mini capstone project analyzes a film rental dataset to uncover rental patterns, identify popular films, evaluate store performance, and provide actionable business insights. Key findings include peak rental months (April and May), most rented films, busiest rental hours, and top-performing categories like Sports, Animation, Action, and Sci-Fi. The analysis leveraged SQL and Excel for data extraction and visualization, highlighting opportunities for targeted promotions, optimized stocking, and strategic marketing.',
+      link: 'https://github.com/Saidireddy25/Projects/tree/main/Mini%20Capstone',
+      image: '/Movie.webp',
+      techStack: ['MS Excel','Data analysis','Data story telling'],
+      tag: 'Film Industry',
+    }, 
   ];
 
   const visibleProjects = showMore ? projectList : projectList.slice(0, 3);
